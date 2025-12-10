b0ee906214554e8ae466ae028e7fd9863f27d58d
Cavva-cpu <sutovsavelij236@gmail.com>
2025-12-10 21:41:20 +0500

    Update main.php (method POST)

--- main.php
+++ main.php
@@ -8,25 +8,27 @@ require __DIR__ . '/../REST API/vendor/autoload.php';
 $app = AppFactory::create();
 $app->addBodyParsingMiddleware();
 
-$app->get('/', function (Request $request, Response $response) {
-    $response->getBody()->write("work");
-
-    return $response;
-});
-
 $app->get('/user', function (Request $request, Response $response) {
     $response->getBody()->write("Сайт работает");
     return $response;
 });
 $app->post('/user/{id}', function (Request $request, Response $response) {
-    $id = $request->getQueryParams();
-    $userId = $id['id'];
-    $response->getBody()->write($userId);
+    $data_request = $request->getQueryParams();
+    $file = 'repository/Reposytory.json';
+    if (file_exists($file)) {
+        $json_string = file_get_contents($file);
+        $data = json_decode($json_string, true);
+    };
+    $new_item = [
+        'login' => $data_request['login'],
+        'password' => $data_request['password']
+    ];
+    $data['PC'] = $new_item;
+    $updated_json = json_encode($data, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+    file_put_contents($file, $updated_json);
+    $response->getBody()->write("Готово");
     return $response;
 });
-$app->get('/user/{id}', function (Request $request, Response $response, array $args) {
-    $userid = $args['id'];
-    $response->getBody()->write("Ваш id:" . $userid);
-    return $response;
-});
+
+
 $app->run();
