e729784031a721ddd0b8dbfcbafa0b7868dca03a
Cavva-cpu <sutovsavelij236@gmail.com>
2025-12-19 20:51:41 +0500

    code cleaning

--- main.php
+++ main.php
@@ -8,40 +8,37 @@ require __DIR__ . '/vendor/autoload.php';
 
 $app = AppFactory::create();
 $app->addBodyParsingMiddleware();
+const file = 'repository/Repository.json';
 
 $app->get('/user', function (Request $request, Response $response) {
-    $data_request = $request->getQueryParams();
     $data = new RepositoryJSON();
-    $response->getBody()->write($data->Get($data_request));
+    $response->getBody()->write($data->Get());
     return $response;
 });
 
 
 
 $app->post('/user/{id}', function (Request $request, Response $response) {
-    $file = 'repository/Repository.json';
     $data_request = $request->getQueryParams();
     $data = new RepositoryJSON();
-    file_put_contents($file,$data->Post($data_request));
+    file_put_contents(file,$data->Post($data_request));
     $response->getBody()->write("Записка была успешно записана!");
     return $response;
 });
 
 $app->delete('/user/{id}', function(Request $request, Response $response, array $args){
     $note_id = $args['id'];
-    $file = 'repository/Repository.json';
     $data = new RepositoryJSON();
-    file_put_contents($file, $data->delete($note_id));
+    file_put_contents(file, $data->delete($note_id));
     $response->getBody()->write('Заметка Успешно Удалена');
     return $response;
 });
 
 $app->put('/user/{id}', function(Request $request, Response $response, array $args){
     $note_id = $args['id'];
-    $file = 'repository/Repository.json';
     $data_request = $request->getQueryParams();
     $data = new RepositoryJSON();
-    file_put_contents($file, $data->Put($data_request,$note_id));
+    file_put_contents(file, $data->Put($data_request,$note_id));
     $response->getBody()->write('Заметка Успешно Заменена');
     return $response;
 });
--- repository/RepositoryJSON.php
+++ repository/RepositoryJSON.php
@@ -2,61 +2,48 @@
 namespace methods;
 
 class RepositoryJSON{
+    const file = 'repository/Repository.json';
     public function Post($data_request){
-    $file = 'repository/Repository.json';
-
-    if (file_exists($file)) {
-        $json_string = file_get_contents($file);
+    if (file_exists(file)) {
+        $json_string = file_get_contents(file);
         $data = json_decode($json_string, true);
     }
-        
-    $new_item = [
+
+    $data[] = $new_item = [
         'note' => $data_request['note'],
     ];
-
-    $data[] = $new_item;
-
     foreach($data as $index => $note){
         $data_with_id[] = array(
             'id' => $index + 1,
             'note' => $note['note']
         );
     }
-    
-    $updated_json = json_encode($data_with_id, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
 
+    $updated_json = json_encode($data_with_id, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
     return $updated_json;
 }
 
-    public function Get($data_request){
-        $file = 'repository/Repository.json';
-
-        if (file_exists($file)) {
-        $json_string = file_get_contents($file);
+    public function Get(){
+        if (file_exists(file)) {
+        $json_string = file_get_contents(file);
         }
-
         return $json_string;
     }
 
     public function delete ($note_id){
-        $file = 'repository/Repository.json';
-
-        if (file_exists($file)){
-        $json_srting = file_get_contents($file);
+        if (file_exists(file)){
+        $json_srting = file_get_contents(file);
         $data = json_decode($json_srting, true);
         }
 
         unset($data[$note_id - 1]);
-
         $updated_json = json_encode($data, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
         return $updated_json;
     }
 
     public function Put ($data_request, $note_id){
-        $file = 'repository/Repository.json';
-
-        if (file_exists($file)){
-        $json_srting = file_get_contents($file);
+        if (file_exists(file)){
+        $json_srting = file_get_contents(file);
         $data = json_decode($json_srting, true);
         }
     
