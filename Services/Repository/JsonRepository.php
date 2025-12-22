6356d9affd53cffec5198f485e883d8f5fec586a
Cavva-cpu <sutovsavelij236@gmail.com>
2025-12-23 00:10:40 +0500

    Update Service/JsonRepository

--- Services/Repository/JsonRepository.php
+++ Services/Repository/JsonRepository.php
@@ -5,15 +5,53 @@ class A implements RepositoryInterface
 {
     public function Get()
     {
+        if (file_exists(file)) {
+            $json_string = file_get_contents(file);
+        }
+        return $json_string;
     }
     public function Create($data_request)
     {
+        if (file_exists(file)) {
+            $json_string = file_get_contents(file);
+            $data = json_decode($json_string, true);
+        }
+
+        $data[] = $new_item = [
+            'note' => $data_request['note'],
+        ];
+        foreach ($data as $index => $note) {
+            $data_with_id[] = array(
+                'id' => $index + 1,
+                'note' => $note['note']
+            );
+        }
+
+        $updated_json = json_encode($data_with_id, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+        return $updated_json;
     }
     public function Put($data_request, $note_id)
     {
+        if (file_exists(file)) {
+            $json_srting = file_get_contents(file);
+            $data = json_decode($json_srting, true);
+        }
+
+        $data[$note_id - 1]['note'] = $data_request['note'];
+
+        $updated_json = json_encode($data, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+        return $updated_json;
     }
     public function Delete($note_id)
     {
+        if (file_exists(file)) {
+            $json_srting = file_get_contents(file);
+            $data = json_decode($json_srting, true);
+        }
+
+        unset($data[$note_id - 1]);
+        $updated_json = json_encode($data, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+        return $updated_json;
     }
 
 }
